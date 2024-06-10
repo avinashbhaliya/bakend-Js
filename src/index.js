28777619a7656cc6f7ffd8631a08bd1645687fa0
avinashbhaliya <avinashbhaliya159@gmail.com>
2024-06-10 18:30:36 +0530

    Custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is Running on PORT ${process.env.PORT}`);
+        
+    }) 
+})
+.catch((err)=>{
+   console.log("MOngo db connection Failed !!",err);
+
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (reqHandler) => {
+    (req,res,next) => {
+        Promise.resolve(reqHandler(req,res,next)).catch((err)=>{next(err)})  
+    }
+}
+
+
+export {asyncHandler}
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async() => {}
+
+// const asyncHandler  = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message : error.message
+
+//         })
+//     }
+// }
